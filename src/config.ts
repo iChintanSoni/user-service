cd0a2562977a8277189f6671059e9a8f3de0eca5
Chintan Soni <ichintansoni@gmail.com>
2023-09-08 14:32:30 +0530

    * Refactored service names with one mentioned in compose *  Added a script to deploy service individually

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const APP_HOST = process.env.APP_HOST || 'user';
+export const APP_HOST = process.env.APP_HOST || 'user-service';
 export const APP_PORT = process.env.APP_PORT || 3100;
 
 export const USER_SERVICE = 'UserService';
@@ -7,7 +7,7 @@ export const USER_SERVICE_PORT = process.env.USER_SERVICE_PORT || 3101;
 export const ROLE_SERVICE = 'RoleService';
 export const ROLE_SERVICE_PORT = process.env.ROLE_SERVICE_PORT || 3102;
 
-export const DATABASE_HOST = process.env.DATABASE_HOST || 'mysql';
+export const DATABASE_HOST = process.env.DATABASE_HOST || 'localhost';
 export const DATABASE_PORT = process.env.DATABASE_PORT || 3306;
 export const DATABASE_USERNAME = process.env.DATABASE_USERNAME || 'root';
 export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || 'root';
